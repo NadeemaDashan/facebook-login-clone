e5d64c50974d0075368169c00a6fdca5a48b64ee
Dashan Nadeema <dashanwd@gmail.com>
2024-02-18 10:53:17 +0530

    authentication completed

--- facebook-login-clone/src/pages/Login.jsx
+++ facebook-login-clone/src/pages/Login.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import logo from"../assets/logo-facebook.svg"
 import "./login.css"
+import Axios from "axios";
 
 export default function Login(){
+        let url="http://localhost:8080/user/get"
+        const [form,setForm]=React.useState({
+            password:"",
+            email:""
+
+        })
+
+        function handleChange(e){
+            setForm((prevState)=>{
+                return{
+                    ...prevState,
+                    [e.target.name]:e.target.value
+                }
+            })
+        }
+        function submitForm(e){
+            e.preventDefault();
+            Axios.post(url,{username:form.email,password:form.password})
+            .then((res)=>console.log(res))
+        }
+
+
     return(
         <div className="container">
             <div className="paragraph">
@@ -11,13 +34,34 @@ export default function Login(){
             </div>
             <div className="login-container">
                <div className="login-box">
-                    <input id="email" type="text" placeholder="Email address or phone Number"></input>
-                    <input type="password" placeholder="Password"></input>
+                <form onSubmit={(e)=>submitForm(e)}>
+                <input
+                    id="email"
+                    type="text"
+                    placeholder="Email address or phone Number"
+                    value={form.email} 
+                    name="email"
+                    required
+                    onChange={(e)=>handleChange(e)}
+                    >   
+                    </input>
+                    
+                    <input
+                     id="password"
+                    type="password"
+                    placeholder="Password"
+                    value={form.password}
+                    name="password"
+                    required
+                    onChange={(e)=>handleChange(e)}
+                    >
+                    </input>
                     <button id="btn">Log in</button>
                     <label id="forgot-txt">  Forgotten Password</label>
                     <hr></hr>
                     <button id="btn-sign-up">Create new account</button>
                     <p>Create a page for a celebrity brand, or a business</p>
+                </form>
                </div>
             </div>
         </div>
